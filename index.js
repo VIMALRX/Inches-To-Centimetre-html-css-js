8ede414ec082fda91d256de30572e3e47fe85904
VIMALRX <vimalkannan624@gmail.com>
2024-08-20 11:52:32 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function cm(){
+    let a=Number(document.getElementById("one").value);
+    let b=(a*2.54).toFixed(3);
+    document.getElementById("result").textContent=b+"CM";
+    document.getElementById("result").style.color="green";
+}
+function In(){
+    let a=Number(document.getElementById("two").value);
+    let b=(a/2.54).toFixed(3);
+    document.getElementById("result").textContent=b+"I n";
+      document.getElementById("result").style.color="green";
+}
